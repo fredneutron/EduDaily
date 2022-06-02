564bfba3e29c43366b56014b53c53b9891e4f376
fredneutron <adelabu4fred@gmail.com>
2022-06-02 09:45:31 +0100

    add contact mail

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <footer class="text-center">
-        <p style="font-size: 14px;">{{ now()->toArray()['year']}}&nbsp;&nbsp;<i class="fa fa-copyright"></i>&nbsp; copyright | Handcrafted by Babatunde Adelabu</p>
+        <p style="font-size: 14px;">{{ now()->toArray()['year']}}&nbsp;&nbsp;<i class="fa fa-copyright"></i>&nbsp; copyright | <a href="mailto:adelabu4fred@gmail.com?subject=Inquiry about &body=Dear EduDaily, I will like make an inquiry about ">contact us</a> | Handcrafted by Babatunde Adelabu</p>
     </footer>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
